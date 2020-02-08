483ffaaac3975919782db26383f624ecc54ed09f
Jakob Majnek <jakob.majnek@outlook.com>
2020-02-08 11:27:22 +0100

    Add PID tune

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Jakob, Ender-3)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -472,8 +472,8 @@
 #define PID_MAX BANG_MAX // Limits current to nozzle while PID is active (see PID_FUNCTIONAL_RANGE below); 255=full current
 #define PID_K1 0.95      // Smoothing factor within any PID loop
 #if ENABLED(PIDTEMP)
-  //#define PID_EDIT_MENU         // Add PID editing to the "Advanced Settings" menu. (~700 bytes of PROGMEM)
-  //#define PID_AUTOTUNE_MENU     // Add PID auto-tuning to the "Advanced Settings" menu. (~250 bytes of PROGMEM)
+  #define PID_EDIT_MENU         // Add PID editing to the "Advanced Settings" menu. (~700 bytes of PROGMEM)
+  #define PID_AUTOTUNE_MENU     // Add PID auto-tuning to the "Advanced Settings" menu. (~250 bytes of PROGMEM)
   //#define PID_DEBUG             // Sends debug data to the serial port.
   //#define PID_OPENLOOP 1        // Puts PID in open loop. M104/M140 sets the output power from 0 to PID_MAX
   //#define SLOW_PWM_HEATERS      // PWM with very low frequency (roughly 0.125Hz=8s) and minimum state time of approximately 1s useful for heaters driven by a relay
@@ -485,13 +485,13 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
     // E3D Hemera
-  // #define DEFAULT_Kp 26.81
-  // #define DEFAULT_Ki 2.21
-  // #define DEFAULT_Kd 81.27
+  #define DEFAULT_Kp 31.91
+  #define DEFAULT_Ki 2.80
+  #define DEFAULT_Kd 90.81
 
-  #define DEFAULT_Kp 34.98
-  #define DEFAULT_Ki 3.83
-  #define DEFAULT_Kd 79.92
+  // #define DEFAULT_Kp 34.98
+  // #define DEFAULT_Ki 3.83
+  // #define DEFAULT_Kd 79.92
 
   // Ultimaker
   // #define DEFAULT_Kp 22.2
@@ -527,7 +527,7 @@
  * heater. If your configuration is significantly different than this and you don't understand
  * the issues involved, don't use bed PID until someone else verifies that your hardware works.
  */
-#define PIDTEMPBED
+//#define PIDTEMPBED
 
 //#define BED_LIMIT_SWITCHING
 
@@ -545,14 +545,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  // #define DEFAULT_bedKp 10.00
-  // #define DEFAULT_bedKi .023
-  // #define DEFAULT_bedKd 305.4
-
-  //Ender 3 Pro with Steel flex sheet and stock heater
-  #define DEFAULT_bedKp 97.23
-  #define DEFAULT_bedKi 15.96
-  #define DEFAULT_bedKd 394.96
+  #define DEFAULT_bedKp 10.00
+  #define DEFAULT_bedKi .023
+  #define DEFAULT_bedKd 305.4
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
