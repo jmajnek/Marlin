7bd931638ecce319847224d2e981269f15ffbf84
Jakob Majnek <jakob.majnek@outlook.com>
2020-03-14 10:44:33 +0100

    Minor changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -740,7 +740,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 403.5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 407 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -951,9 +951,9 @@
  *   T |           | H
  *     |    (-)    | T
  *     |           |
- * 
- * 
- * 
+ *
+ *
+ *
  *     O-- FRONT --+
  *   (0,0)
  *
